5b62353765a248f13930bf3f9a2b75b92783e61a
anagxndin <anapaulagondinbastos@gmail.com>
2023-12-06 14:17:10 -0300

    APIpokedex

--- index.js
+++ index.js
@@ -23,3 +23,70 @@ app.listen(porta, ()=> {
     console.log(`Rodando em http://localhost:${porta}`)
 })
 
+/*tiposRetorna todos os tipos dos Pokémon.
+Retorno: lista chamada tipos.
+Exemplo:
+https://localhost:3000/v1/tipos
+
+/regioes
+Retorna todas as regiões dos Pokémon.
+Retorno: lista chamada regioes.
+Exemplo:
+https://localhost:3000/v1/regioes
+
+/pokemon/nome/{nomeDoPokemon}
+Retorna um único Pokémon com o nome informado.
+Retorno: objeto chamado pokemon.
+Parâmetros:
+• nomeDoPokemon: (string) nome do Pokémon a ser retornado.
+Exemplo:
+https://localhost:3000/v1/nome/pikachu
+Retorna as informações do Pikachu.
+
+/pokemon/numero/{numeroDoPokemon}
+Retorna um único Pokémon com o número informado.
+Retorno: objeto chamado pokemon.
+Parâmetros:
+• numeroDoPokemon: (int) numero do Pokémon na Pokédex, a ser retornado.
+Exemplo:
+https://localhost:3000/v1/numero/25
+Retorna as informações do Pikachu.
+
+/pokemon/tipo/{nomeDoTipo}/{quantidade}/{pagina}
+Retorna uma lista de Pokémon do tipo informado.
+Retorno: lista chamada pokemon.
+Parâmetros:
+• nomeDoTipo: (string) tipo dos Pokémon a serem retornados.
+• quantidade: (int) quantidade de Pokémon retornados (máximo de 50 por vez).
+• pagina: (int) número da página de resultados.
+Exemplo:
+https://localhost:3000/v1/tipo/electric/10/1
+Retorna os 10 primeiros Pokémon do tipo elétrico (eletric).
+
+/pokemon/geracao/{numeroDaGeracao}/{quantidade}/{pagina}
+Retorna uma lista de Pokémon que pertencem à geração informada.
+Retorno: lista chamada pokemon.
+Parâmetros:
+• numeroDaGeracao: (int) número da geração dos Pokémon a serem retornados.
+• quantidade: (int) quantidade de Pokémon retornados (máximo de 50 por vez).
+• pagina: (int) número da página de resultados.
+Exemplo:
+https://localhost:3000/v1/geracao/3/10/1
+Retorna os 10 primeiros Pokémon da 3ª Geração.
+
+/pokemon/lista/{numeroDaGeracao}
+Retorna uma lista com apenas nome e número dos Pokémon da região informada.
+Retorno: lista de dicionários chamada pokemon.
+Parâmetros:
+• numeroDaGeracao: (int) número da geração dos Pokémon a serem retornados.
+Exemplo:
+https://localhost:3000/v1/pokemon/lista/1
+Retorna uma lista com apenas nome e número de todos os Pokémon da 1ª Geração.
+
+/pokemon/lista/
+Retorna uma lista com apenas nome e número de todos os Pokémon.
+Retorno: lista chamada pokemon.
+Exemplo:
+https://localhost:3000/v1/pokemon/lista
+Retorna uma lista com apenas nome e número de todos os Pokémon
+/*
